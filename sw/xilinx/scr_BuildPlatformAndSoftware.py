8ee65f1754155d63548eac247a118d105e3241e2
Javier Contreras <javierc@fnal.gov>
2026-06-21 17:03:53 -0500

    feat: Support different CPU targets

--- sw/xilinx/scr_BuildPlatformAndSoftware.py
+++ sw/xilinx/scr_BuildPlatformAndSoftware.py
@@ -9,7 +9,7 @@ import platform as os_platform
 root = (Path(__file__).parent / "../../..").resolve()
 
 try:
-    xsa_path = next(root.glob("*.xsa"))
+    xsa_path = next(root.glob("_outputs/fw/latest/*.xsa"))
 except StopIteration:
     raise Exception("Failed finding .xsa file. Fix fw flow first!")
 
@@ -18,33 +18,34 @@ client = vitis.create_client()
 
 # Configure workspace
 date = datetime.now().strftime("%Y%m%d%I%M%S")
-workspace = root / f'_build/sw/vitis_{date}/'
+workspace = root / f"_build/sw/vitis_{date}/"
 
-if (os.path.isdir(workspace)):
+if os.path.isdir(workspace):
     shutil.rmtree(workspace)
 
 client.set_workspace(workspace)
 
 # Configure platform
 project_name = os.getenv("PROJECT_NAME")
-if project_name == '':
-    project_name = 'NoName'
+if project_name == "":
+    project_name = "NoName"
 
 platform_name = f"plat_{project_name}"
 print("Creating platform")
 print(xsa_path)
-platform = client.create_platform_component(
-    name = platform_name,
-    hw_design = xsa_path
-)
+platform = client.create_platform_component(name=platform_name, hw_design=xsa_path)
 platform.report()
 
 print("Creating domain")
 domain_name = "standalone_a53_0"
-standalone_a53_0 = platform.add_domain(
-    name = domain_name,
-    cpu = "psu_cortexa53_0",
-    os = "standalone")
+cpu_name = os.getenv("CPU_NAME")
+if cpu_name == "":
+    exc = Exception("Add the expected CPU name to your project.yaml")
+    exc.add_note(
+        "E.g. psu_cortexa53_0 or microblaze_0_microblaze_0 (check your block design)"
+    )
+    raise exc
+standalone_a53_0 = platform.add_domain(name=domain_name, cpu=cpu_name, os="standalone")
 standalone_a53_0.report()
 
 # Check that domain was created
@@ -58,9 +59,7 @@ platform.build()
 print("Creating app")
 platform_xpfm = client.find_platform_in_repos(platform_name)
 app_component = client.create_app_component(
-    name = f'sw_{project_name}',
-    platform = platform_xpfm,
-    domain = domain_name
+    name=f"sw_{project_name}", platform=platform_xpfm, domain=domain_name
 )
 
 app_component.get_app_config()
@@ -69,13 +68,13 @@ src_dir = root / "sw" / "src"
 files = [f.name for f in src_dir.glob("**/*.[ch]*")]
 print("Importing files")
 print(files)
-app_component.import_files(
-    from_loc = src_dir,
-    files = files,
-    dest_dir_in_cmp = 'src'
-)
+app_component.import_files(from_loc=src_dir, files=files, dest_dir_in_cmp="src")
+
 
 print("Building app")
+linker = app_component.get_ld_script()
+linker.regenerate()
+
 app_component.report()
 app_component.build()
 
--- sw/xilinx/scr_BuildSoftware.sh
+++ sw/xilinx/scr_BuildSoftware.sh
@@ -5,6 +5,7 @@ PROJECT_YAML=${rootdir}/project.yaml
 
 export PROJECT_NAME=$(yq '.* | .project_name_short' ${PROJECT_YAML})
 export PART_NUMBER=$(yq '.* | .part_number' ${PROJECT_YAML})
+export CPU_NAME=$(yq '.* | .cpu_name' ${PROJECT_YAML})
 
 export VITIS_VERSION=2024.1
 export VITIS_DIR=/data/Xilinx/Vitis/${VITIS_VERSION}
